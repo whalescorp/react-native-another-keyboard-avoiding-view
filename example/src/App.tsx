94d36ece04a3f2ec8fd65e98437e26e3dab12571
Dan Volkov <hi@dvlkv.com>
2023-04-26 21:37:33 +0400

    chore: add webview

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,24 +1,37 @@
 import * as React from 'react';
 
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { AnotherKeyboardAvoidingViewView } from 'react-native-another-keyboard-avoiding-view';
+import { WebView } from 'react-native-webview';
+
+// function NonWebViewContent() {
+//   return (
+//     <>
+//       <View style={{ padding: 30, width: '100%', alignItems: 'center' }}>
+//         <TextInput
+//           placeholder="Input"
+//           style={{
+//             width: '100%',
+//             borderWidth: 1,
+//             borderColor: '#000',
+//             padding: 4,
+//           }}
+//         ></TextInput>
+//       </View>
+//       <Text>Low text at keyboard</Text>
+//     </>
+//   );
+// }
 
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
       <AnotherKeyboardAvoidingViewView style={styles.container}>
-        <View style={{ padding: 30, width: '100%', alignItems: 'center' }}>
-          <TextInput
-            placeholder="Input"
-            style={{
-              width: '100%',
-              borderWidth: 1,
-              borderColor: '#000',
-              padding: 4,
-            }}
-          ></TextInput>
-        </View>
-        <Text>Low text at keyboard</Text>
+        {/* <NonWebViewContent /> */}
+        <WebView
+          style={{ flex: 1 }}
+          source={{ uri: 'https://next.zenpay.org' }}
+        />
       </AnotherKeyboardAvoidingViewView>
     </View>
   );
